d0d7320dc0d32cbc7d339defa7f029f1ce8919fa
Matthew Scharley <matt.scharley@gmail.com>
2016-08-04 22:11:39 +1000

    Add support for nextInt()

--- src/main/scala/Benchmark.scala
+++ src/main/scala/Benchmark.scala
@@ -1,11 +1,10 @@
 import org.scalameter._
 
 object Benchmark extends App {
-  val ITERATIONS = 10000000
+  val ITERATIONS = 1000000
 
   private val measurer = config(
-    Key.exec.benchRuns -> 20,
-    Key.verbose -> true
+    Key.exec.benchRuns -> 20
   ) withWarmer {
     new Warmer.Default
   } withMeasurer {
@@ -15,22 +14,21 @@ object Benchmark extends App {
   val scalaRand = new java.util.Random
   val xoroRand = new xoroshiro128
 
-  val timeJavaLong = measurer measure {
-    for (i <- 0 until ITERATIONS) yield scalaRand.nextLong()
-  }
+  val timeJavaLong = measurer measure { for (i <- 0 until ITERATIONS) yield scalaRand.nextLong() }
+  val timeXoroLong = measurer measure { for (i <- 0 until ITERATIONS) yield xoroRand.nextLong() }
+  val longImprovement = (1 - timeXoroLong.value / timeJavaLong.value) * 100
 
-  val timeXoroLong = measurer measure {
-    for (i <- 0 until ITERATIONS) yield xoroRand.nextLong()
-  }
+  val timeJavaInt = measurer measure { for (i <- 0 until ITERATIONS) yield scalaRand.nextInt() }
+  val timeXoroInt = measurer measure { for (i <- 0 until ITERATIONS) yield xoroRand.nextInt() }
+  val intImprovement = (1 - timeXoroInt.value / timeJavaInt.value) * 100
 
-  val timeJavaBool = measurer measure {
-    for (i <- 0 until ITERATIONS) yield scalaRand.nextBoolean()
-  }
+  val timeJavaBool = measurer measure { for (i <- 0 until ITERATIONS) yield scalaRand.nextBoolean() }
+  val timeXoroBool = measurer measure { for (i <- 0 until ITERATIONS) yield xoroRand.nextBoolean() }
+  val boolImprovement = (1 - timeXoroBool.value / timeJavaBool.value) * 100
 
-  val timeXoroBool = measurer measure {
-    for (i <- 0 until ITERATIONS) yield xoroRand.nextBoolean()
-  }
-
-  println(s"Long: java.util.Random - $timeJavaLong, xoroshiro128 - $timeXoroLong")
-  println(s"Boolean: java.util.Random - $timeJavaBool, xoroshiro128 - $timeXoroBool")
+  println(s"Benchmarked $ITERATIONS iterations:")
+  println("")
+  println(s"Long:    java.util.Random - $timeJavaLong, xoroshiro128 - $timeXoroLong; $longImprovement% improvement")
+  println(s"Int:     java.util.Random - $timeJavaInt, xoroshiro128 - $timeXoroInt; $intImprovement% improvement")
+  println(s"Boolean: java.util.Random - $timeJavaBool, xoroshiro128 - $timeXoroBool; $boolImprovement% improvement")
 }
