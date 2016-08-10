87132c6b041c2d5cb1c80f58accb815f534a426c
Matthew Scharley <matt.scharley@gmail.com>
2016-08-11 00:24:31 +1000

    Implement using Iterators

--- src/main/scala/splitmix64.scala
+++ src/main/scala/splitmix64.scala
@@ -4,8 +4,10 @@ object splitmix64 {
   def apply(seed : Long) = new splitmix64(seed)
 }
 
-class splitmix64 private (private var seed : Long) {
-  def nextLong() : Long = {
+class splitmix64 private (private var seed : Long) extends Iterator[Long] {
+  override def hasNext: Boolean = true
+
+  override def next(): Long = {
     seed += 0x9E3779B97F4A7C15L
     var z = (seed ^ (seed >>> 30)) * 0xBF58476D1CE4E5B9L
     z = (z ^ (z >>> 27)) * 0x94D049BB133111EBL
--- src/main/scala/xoroshiro128.scala
+++ src/main/scala/xoroshiro128.scala
@@ -1,20 +1,25 @@
 import java.lang.Long.{rotateLeft, signum => signLong}
 
+import scala.collection.AbstractIterator
+
 object xoroshiro128 {
   def apply(seedLo : Long, seedHi : Long) : xoroshiro128 = new xoroshiro128(seedLo, seedHi)
   def apply(seed : Array[Long]) = new xoroshiro128(seed(0), seed(1))
   def apply(seed : Long) = {
     val r = splitmix64(seed)
-    new xoroshiro128(r.nextLong(), r.nextLong())
+    new xoroshiro128(r.next(), r.next())
   }
   def apply() = {
     val r = splitmix64()
-    new xoroshiro128(r.nextLong(), r.nextLong())
+    new xoroshiro128(r.next(), r.next())
   }
 }
 
-class xoroshiro128 private (private var seedLo : Long, private var seedHi : Long) {
-  def nextLong() : Long = {
+class xoroshiro128 private (private var seedLo : Long, private var seedHi : Long) extends AbstractIterator[Long] {
+  self =>
+
+  override def hasNext : Boolean = true
+  override def next() : Long = {
     val result : Long = seedLo + seedHi
     val s1     : Long = seedHi ^ seedLo
 
@@ -24,19 +29,23 @@ class xoroshiro128 private (private var seedLo : Long, private var seedHi : Long
     result
   }
 
-  private var intProgress : Long = 0
-  def nextInt() : Int = {
-    if (intProgress == 0) { intProgress = nextLong() }
+  def asInt : Iterator[Int] = new AbstractIterator[Int] {
+    private var intProgress : Long = 0
 
-    val result : Int = (intProgress & 0xffffffff).toInt
-    intProgress = intProgress >>> 32
+    override def hasNext : Boolean = self.hasNext
+    override def next() : Int = {
+      if (intProgress == 0) { intProgress = self.next() }
 
-    result
+      val result : Int = (intProgress & 0xffffffff).toInt
+      intProgress = intProgress >>> 32
+
+      result
+    }
   }
 
   private var byteProgress : Long = 0
-  def nextByte() : Byte = {
-    if (byteProgress == 0) { byteProgress = nextLong() }
+  protected def nextByte() : Byte = {
+    if (byteProgress == 0) { byteProgress = next() }
 
     val result : Byte = (byteProgress & 0xff).toByte
     byteProgress = byteProgress >>> 8
@@ -47,5 +56,13 @@ class xoroshiro128 private (private var seedLo : Long, private var seedHi : Long
   def nextBytes(bytes : Array[Byte]) : Unit =
     for (i <- bytes.indices) { bytes(i) = nextByte() }
 
-  def nextBoolean() : Boolean = signLong(nextLong()) == 1
+  def asByte : Iterator[Byte] = new AbstractIterator[Byte] {
+    override def hasNext: Boolean = self.hasNext
+    override def next(): Byte = self.nextByte()
+  }
+
+  def asBoolean : Iterator[Boolean] = new AbstractIterator[Boolean] {
+    override def hasNext: Boolean = self.hasNext
+    override def next(): Boolean = signLong(self.next()) == 1
+  }
 }
