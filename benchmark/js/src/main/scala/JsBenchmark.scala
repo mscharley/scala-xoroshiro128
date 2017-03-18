f994388e320cac1d6f36ef10d43a9b390852c887
Matthew Scharley <matt.scharley@gmail.com>
2017-03-18 11:22:25 +1100

    Fix inability to run the JS benchmark

--- benchmark/js/src/main/scala/JsBenchmark.scala
+++ benchmark/js/src/main/scala/JsBenchmark.scala
@@ -1,4 +1,7 @@
+import scala.scalajs.js.JSApp
 
-object JsBenchmark extends Benchmark {
+object JsBenchmark extends Benchmark with JSApp {
   val measurer : Measurer = new SimpleMeasurer(BENCHES)
+
+  override def main(): Unit = main(Array())
 }
